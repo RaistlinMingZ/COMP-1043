2c9d8f401e494250825b6bc6386864e025bb6d62
RaistlinMingZ <raistlinming@outlook.com>
2021-09-14 19:47:18 +0930

    Test

---
+++ Welcome.py
@@ -0,0 +1,14 @@
+#
+# File: welcome.py
+# Author: Ming Zeng
+# Student Id: 110355502
+# Email Id: zenmy005@mymail.unisa.edu.au
+# Date: today's date
+# Description: Displays a welcome message to the screen.
+# This is my own work as defined by the University's
+# Academic Misconduct policy.
+#
+# Display welcome message to the screen
+print('Author: Ming Zeng')
+print('Email Id: zenmy005@mymail.unisa.edu.au')
+print('This is my own work as defined by the University’s Academic Misconduct Policy.')
